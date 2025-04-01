3d575b57507c62eaf9e4b2d26a86b66ca1ed84ca
carolinaadamek <wi23b125@technikum-wien.at>
2025-04-01 11:56:55 +0200

    projectInterface beginning

--- ui-app/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module uiapp.uiapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires jdk.incubator.vector;
-
-
-    opens uiapp.uiapp to javafx.fxml;
-    exports uiapp.uiapp;
-}
