d440bb6e1297ee9737cbe7df19b1acbfbdeaaa57
ChristophBout <wi23b005@technikum-wien.at>
2025-06-20 12:08:36 +0200

    fixed compiling error in EnergyProducerMain

--- EnergyProducer/src/main/java/org/example/EnergyProducerMain.java
+++ EnergyProducer/src/main/java/org/example/EnergyProducerMain.java
@@ -8,14 +8,12 @@ import org.slf4j.LoggerFactory;
 public class EnergyProducerMain {
     public static void main(String[] args) throws Exception {
         EnergyProducerService producer = new EnergyProducerService("unused", "energy_queue", "localhost");
-        logger.info("Main started");
+        logger.info("EnergyProducer started");
 
         while (true) {
             producer.sendEnergyReading();
             Thread.sleep(5000);
         }
-
-
     }
     private static final Logger logger = LoggerFactory.getLogger(EnergyProducerMain.class);
 }
--- EnergyUser/src/main/java/org/example/EnergyUserMain.java
+++ EnergyUser/src/main/java/org/example/EnergyUserMain.java
@@ -1,14 +1,20 @@
 package org.example;
 
 import org.example.service.EnergyUserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class EnergyUserMain {
     public static void main(String[] args) throws Exception {
         EnergyUserService user = new EnergyUserService("unused", "energy_queue", "localhost");
+        logger.info("EnergyUser started");
 
         while (true) {
             user.sendEnergyUsage();  // Send every few seconds
             Thread.sleep(5000);
         }
     }
+    private static final Logger logger = LoggerFactory.getLogger(EnergyUserMain.class);
+
 }
