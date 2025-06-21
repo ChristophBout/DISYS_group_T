1b1743cf27b864ae5aeb076f407e1fba10c0322b
ChristophBout <wi23b005@technikum-wien.at>
2025-06-21 18:22:06 +0200

    added random 1-5s delays between message queue sends

--- EnergyProducer/src/main/java/org/example/EnergyProducerMain.java
+++ EnergyProducer/src/main/java/org/example/EnergyProducerMain.java
@@ -3,17 +3,21 @@ package org.example;
 import org.example.service.EnergyProducerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import java.util.Random;
 
 public class EnergyProducerMain {
+    private static final Logger logger = LoggerFactory.getLogger(EnergyProducerMain.class);
+    private static final Random random = new Random();
+
     public static void main(String[] args) throws Exception {
         EnergyProducerService producer = new EnergyProducerService("unused", "energy_queue", "localhost");
         logger.info("EnergyProducer started");
 
         while (true) {
             producer.sendEnergyReading();
-            Thread.sleep(5000);
+            // Random delay between 1 and 5 seconds
+            int delay = 1000 + random.nextInt(4000); // 0–3999 + 1000
+            Thread.sleep(delay);
         }
     }
-    private static final Logger logger = LoggerFactory.getLogger(EnergyProducerMain.class);
 }
--- EnergyUser/src/main/java/org/example/EnergyUserMain.java
+++ EnergyUser/src/main/java/org/example/EnergyUserMain.java
@@ -3,18 +3,21 @@ package org.example;
 import org.example.service.EnergyUserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import java.util.Random;
 
 public class EnergyUserMain {
+    private static final Logger logger = LoggerFactory.getLogger(EnergyUserMain.class);
+    private static final Random random = new Random();
+
     public static void main(String[] args) throws Exception {
         EnergyUserService user = new EnergyUserService("unused", "energy_queue", "localhost");
         logger.info("EnergyUser started");
 
         while (true) {
-            user.sendEnergyUsage();  // Send every few seconds
-            Thread.sleep(5000);
+            user.sendEnergyUsage();
+            // Random delay between 1 and 5 seconds
+            int delay = 1000 + random.nextInt(4000); // 0–3999 + 1000
+            Thread.sleep(delay);
         }
     }
-    private static final Logger logger = LoggerFactory.getLogger(EnergyUserMain.class);
-
 }
